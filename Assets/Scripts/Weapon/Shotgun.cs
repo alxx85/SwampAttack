3507826908cfc54584b63b80cfa9d2cc8ddc5e3e
Aleksandr Bashkirov <alxx85@mail.ru>
2022-02-03 07:16:12 +0700

    Изменена цена покупки

---
+++ Assets/Scripts/Weapon/Shotgun.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgun : Weapon
+{
+    [SerializeField] private int _bulletInShoot = 3;
+    private float range = 0.2f;
+
+    public override void Shoot(Transform shootPoint)
+    {
+        for (int i = 0; i < _bulletInShoot; i++)
+        {
+            Vector3 currentShootPoint = shootPoint.position;
+            currentShootPoint.y += Random.Range(-range, range);
+            Instantiate(Bullet, currentShootPoint, Quaternion.identity);
+        }
+    }
+}
